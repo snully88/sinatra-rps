b89907ff4c0bae67ebbab7cc6ba1749f6928beb3
Nathalie Lam <114606547+snully88@users.noreply.github.com>
2024-10-20 16:35:48 +0000

    chchchanges

--- app.rb
+++ app.rb
@@ -13,13 +13,13 @@ get ("/rock") do
 
   comp_move = moves.sample
 
-if comp_move == "rock"
-  outcome = "tied"
-elsif comp_move == "paper"
-  outcome == "lost"
-else
-  outcome = "won"
+  if comp_move == "rock"
+    @outcome = "tied"
+  elsif comp_move == "paper"
+    @outcome == "lost"
+  else
+    @outcome = "won"
 
-erb(:zebra)
-
+  erb(:zebra)
+  end
 end
